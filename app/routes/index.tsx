fdc36026369384b095e79663c40b26509b8e2dc1
maxhannan <maxhannan@substanceseven.com>
2023-04-01 18:35:31 -0500

    page nav

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,13 +3,16 @@ import {
   AdjustmentsHorizontalIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/solid";
+import { redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 
 import IconTextField from "~/components/forms/IconTextField";
 import AppBar from "~/components/navigation/AppBar";
 import BottomNav from "~/components/navigation/BottomNav";
 import RecipeFeed from "~/components/recipefeed/RecipeFeed";
-
+export const loader = () => {
+  return redirect("/app/recipes");
+};
 export default function Index() {
   return (
     <div className=" px-4">
@@ -47,7 +50,7 @@ export default function Index() {
       <div className="pb-16 ">
         <RecipeFeed />
       </div>
-      <Outlet />
+
       <BottomNav />
     </div>
   );
