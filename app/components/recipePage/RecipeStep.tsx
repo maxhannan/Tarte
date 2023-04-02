f06ce0dcf5697591b7db13653fc1f31a5d65d09e
maxhannan <maxhannan@substanceseven.com>
2023-04-01 22:23:32 -0500

    Allergies

--- app/components/recipePage/RecipeStep.tsx
+++ app/components/recipePage/RecipeStep.tsx
@@ -14,7 +14,7 @@ const RecipeStep = ({
       className={`${
         checked
           ? "dark:bg-neutral-700 bg-neutral-300 border-l-8 border-l-green-500"
-          : "dark:bg-neutral-800 border-l-8 border-l-pink-500"
+          : "dark:bg-neutral-800 border-l-8 border-l-red-500"
       }  bg-neutral-200 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4`}
     >
       <h5 className="text-2xl mb-2">Step {stepNum}</h5>
--- app/routes/app/recipes/$id.tsx
+++ app/routes/app/recipes/$id.tsx
@@ -2,6 +2,19 @@ import { Transition } from "@headlessui/react";
 import AppBar from "~/components/navigation/AppBar";
 import RecipeStep from "~/components/recipePage/RecipeStep";
 
+const Allergens = [
+  "Not Vegetarian",
+  "Not Vegan",
+  "Dairy",
+  "Eggs",
+  "Fish",
+  "Shellfish",
+  "Tree nuts",
+  "Peanuts",
+  "Soy",
+  "Not Halal",
+  "Not Kosher",
+];
 const RecipePage = () => {
   return (
     <div className="mb-28">
@@ -73,6 +86,27 @@ const RecipePage = () => {
               </tr>
             </tbody>
           </table>
+        </div>
+      </Transition>
+      <Transition
+        enter="transition-all transform  ease-in-out  duration-500"
+        enterFrom=" opacity-0 translate-y-full "
+        enterTo=" opacity-100 translate-y-0"
+        leave="transition ease-in duration-400"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        appear
+        show
+      >
+        <div className="flex mt-4 gap-2 flex-wrap">
+          {Allergens.map((a) => (
+            <div
+              key={a}
+              className=" bg-red-500 p-2 px-3 rounded-r-3xl font-light rounded-l-md rounded-bl-3xl text-base text-neutral-100 dark:text-neutral-100 "
+            >
+              {a}
+            </div>
+          ))}
         </div>
       </Transition>
       <Transition
