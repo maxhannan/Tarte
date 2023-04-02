6bb7407808886f9d54c2373725ab9088c1f8f56b
maxhannan <maxhannan@substanceseven.com>
2023-04-01 21:59:19 -0500

    steps

--- app/routes/app/recipes/$id.tsx
+++ app/routes/app/recipes/$id.tsx
@@ -3,7 +3,7 @@ import AppBar from "~/components/navigation/AppBar";
 
 const RecipePage = () => {
   return (
-    <>
+    <div className="mb-28">
       <AppBar page={"Compressed Watermelon"} textSize="text-2xl" />
       <Transition
         enter="transition-all transform  ease-in-out  duration-500"
@@ -74,7 +74,54 @@ const RecipePage = () => {
           </table>
         </div>
       </Transition>
-    </>
+      <Transition
+        enter="transition-all transform  ease-in-out  duration-500"
+        enterFrom=" opacity-0 translate-y-full "
+        enterTo=" opacity-100 translate-y-0"
+        leave="transition ease-in duration-400"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        appear
+        show
+      >
+        <div className="dark:bg-neutral-700 border-l-8 border-l-green-500 bg-neutral-200 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4">
+          <h5 className="text-2xl mb-2">Step One</h5>
+          <p className="text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quos est totam voluptates veniam magni fugiat ad. Deleniti fuga ex
+            rem? Deleniti alias labore, similique maxime expedita modi quo
+            ratione.
+          </p>
+        </div>
+        <div className="dark:bg-neutral-800 bg-neutral-200 border-l-8 border-l-pink-500 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4">
+          <h5 className="text-2xl mb-2">Step Two</h5>
+          <p className="text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="dark:bg-neutral-800 bg-neutral-200 border-l-8 border-l-pink-500 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4">
+          <h5 className="text-2xl mb-2">Step Three</h5>
+          <p className="text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="dark:bg-neutral-800 bg-neutral-200 border-l-8 border-l-pink-500 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4">
+          <h5 className="text-2xl mb-2">Step Four</h5>
+          <p className="text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="dark:bg-neutral-800 border-l-8 border-l-pink-500 bg-neutral-200 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4">
+          <h5 className="text-2xl mb-2">Step Five</h5>
+          <p className="text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quos est totam voluptates veniam magni fugiat ad. Deleniti fuga ex
+            rem? Deleniti alias labore, similique maxime expedita modi quo
+            ratione.
+          </p>
+        </div>
+      </Transition>
+    </div>
   );
 };
 
