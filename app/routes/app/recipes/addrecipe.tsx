5f4e325af336e721bbc4dde3b6783d1dcaf6ac62
maxhannan <maxhannan@substanceseven.com>
2023-04-02 18:34:01 -0500

    text area

--- app/routes/app/recipes/addrecipe.tsx
+++ app/routes/app/recipes/addrecipe.tsx
@@ -80,7 +80,7 @@ const AddRecipe = () => {
         <TextareaAutosize
           minRows={2}
           placeholder="Add A Step...."
-          className="col-span-5 text-xl p-4 rounded-r-2xl h-16 bg-neutral-200 active:outline-none text-neutral-800 text-light  rounded-l-md rounded-bl-2xl "
+          className="col-span-5 text-xl p-4  dark:placeholder:text-neutral-500 rounded-r-2xl h-16 bg-neutral-200 dark:bg-neutral-800 active:outline-none text-neutral-800 dark:text-neutral-100 text-light  rounded-l-md rounded-bl-2xl "
         />
       </div>
     </div>
