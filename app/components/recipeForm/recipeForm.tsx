f94a1a9d642233512e3edb0bf89b291616aad237
maxhannan <maxhannan@substanceseven.com>
2023-04-02 19:41:56 -0500

    transitions

--- app/components/recipeForm/recipeForm.tsx
+++ app/components/recipeForm/recipeForm.tsx
@@ -11,124 +11,53 @@ import IconButton from "../buttons/IconButton";
 import IconTextField from "../forms/IconTextField";
 
 import SelectBox from "../forms/SelectBox";
+import IngredientSection from "./IngredientsSection";
 
 const RecipeForm = () => {
   return (
-    <Transition
-      enter="transition-all transform  ease-in-out  duration-500"
-      enterFrom=" opacity-0 "
-      enterTo=" opacity-100 "
-      className="grid grid-cols-5 mt-2 mb-32 gap-y-4 gap-x-2"
-      appear
-      show
-    >
-      <div className="col-span-5">
-        <IconTextField Icon={PlusIcon} fieldName="Name" identifier="name" />
-      </div>
-      <div className="col-span-5 relative">
-        <SelectBox />
-      </div>
-      <div className="flex gap-x-2 col-span-5">
-        <div className="grow border-l-8 h-14 inline-flex items-center border-l-red-500   dark:bg-neutral-800 bg-neutral-200 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-700 dark:text-neutral-100  ">
-          <h4 className="text-3xl dark:text-neutral-100 "> Ingredients</h4>
-        </div>
-      </div>
-      <div className="flex gap-x-2 col-span-5">
-        <div className="grow">
+    <div className="flex flex-col gap-y-4">
+      <Transition
+        enter="transition-all transform  ease-in-out  duration-500"
+        enterFrom=" opacity-0 translate-y-full "
+        enterTo=" opacity-100 translate-y-0"
+        leave="transition ease-in duration-400"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        className="flex flex-col gap-y-4"
+        appear
+        show
+      >
+        <div className="col-span-5">
           <IconTextField Icon={PlusIcon} fieldName="Name" identifier="name" />
         </div>
-        <div className="flex justify-center items-center ml-auto">
-          <IconButton
-            Icon={XMarkIcon}
-            size="14"
-            buttonName="deleteRecipe"
-            action={() => console.log("delete")}
-          />
+        <div className="col-span-5 relative">
+          <SelectBox />
         </div>
-      </div>
-      <div className="col-span-5 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-3 relative">
-        <IconTextField Icon={PlusIcon} fieldName="Amt" identifier="name" />
-      </div>
-      <div className="col-span-2 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-5 border-b-4 rounded-sm dark:border-neutral-700"></div>
-      <div className="flex gap-x-2 col-span-5">
-        <div className="grow">
-          <IconTextField Icon={PlusIcon} fieldName="Name" identifier="name" />
+        <div className="flex gap-x-2 col-span-5">
+          <div className=" ">
+            <IconTextField
+              Icon={PlusIcon}
+              fieldName="Yield"
+              identifier="name"
+            />
+          </div>
+          <div className="">
+            <IconTextField Icon={PlusIcon} fieldName="Qty" identifier="name" />
+          </div>
         </div>
-        <div className="flex justify-center items-center ml-auto">
-          <IconButton
-            Icon={XMarkIcon}
-            size="14"
-            buttonName="deleteRecipe"
-            action={() => console.log("delete")}
-          />
-        </div>
-      </div>
-      <div className="col-span-5 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-3 relative">
-        <IconTextField Icon={PlusIcon} fieldName="Amt" identifier="name" />
-      </div>
-      <div className="col-span-2 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-5 border-b-4 rounded-sm dark:border-neutral-700"></div>
-      <div className="flex gap-x-2 col-span-5">
-        <div className="grow">
-          <IconTextField Icon={PlusIcon} fieldName="Name" identifier="name" />
-        </div>
-        <div className="flex justify-center items-center ml-auto">
-          <IconButton
-            Icon={XMarkIcon}
-            size="14"
-            buttonName="deleteRecipe"
-            action={() => console.log("delete")}
-          />
-        </div>
-      </div>
-      <div className="col-span-5 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-3 relative">
-        <IconTextField Icon={PlusIcon} fieldName="Amt" identifier="name" />
-      </div>
-      <div className="col-span-2 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-5 border-b-4 rounded-sm dark:border-neutral-700"></div>
-      <div className="flex gap-x-2 col-span-5">
-        <div className="grow">
-          <IconTextField Icon={PlusIcon} fieldName="Name" identifier="name" />
-        </div>
-        <div className="flex justify-center items-center ml-auto">
-          <IconButton
-            Icon={XMarkIcon}
-            size="14"
-            buttonName="deleteRecipe"
-            action={() => console.log("delete")}
-          />
-        </div>
-      </div>
-      <div className="col-span-5 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-3 relative">
-        <IconTextField Icon={PlusIcon} fieldName="Amt" identifier="name" />
-      </div>
-      <div className="col-span-2 relative">
-        <SelectBox />
-      </div>
-      <div className="col-span-5 border-b-4 rounded-sm dark:border-neutral-700"></div>
-      <div className="col-span-5   h-12 inline-flex border-r-none items-center justify-between px-3 dark:bg-neutral-700 bg-neutral-200 transition-all duration-300 rounded-r-2xl   rounded-l-md rounded-bl-2xl text-lg text-neutral-700 dark:text-neutral-100  ">
-        <h4 className="text-xl dark:text-neutral-100 ">Add Ingredient</h4>
-        <PlusCircleIcon className="h-7 w-7" />
-      </div>
+      </Transition>
+      <Transition
+        enter="transition-all transform  ease-in-out  duration-500"
+        enterFrom=" opacity-0 translate-y-full "
+        enterTo=" opacity-100 translate-y-0"
+        leave="transition ease-in duration-400"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        appear
+        show
+      >
+        <IngredientSection />
+      </Transition>
       <div className="col-span-5 border-l-8 border-r-none h-14 inline-flex items-center border-l-red-500 dark:bg-neutral-800 bg-neutral-200 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-700 dark:text-neutral-100  ">
         <h4 className="text-3xl dark:text-neutral-100 "> Steps</h4>
       </div>
@@ -190,7 +119,7 @@ const RecipeForm = () => {
         <h4 className="text-xl dark:text-neutral-100 ">Add Ingredient</h4>
         <PlusCircleIcon className="h-7 w-7" />
       </div>
-    </Transition>
+    </div>
   );
 };
 
