9b86e6b2948a0487f83920edaa02f2fb0c329d16
maxhannan <maxhannan@substanceseven.com>
2023-04-02 21:28:06 -0500

    outline

--- app/components/forms/CustomTextInput.tsx
+++ app/components/forms/CustomTextInput.tsx
@@ -16,7 +16,7 @@ const CustomTextInput = ({
         value.length > 0
           ? "rounded-r-2xl rounded-l-md rounded-tl-2xl "
           : "rounded-r-2xl rounded-l-md rounded-bl-2xl "
-      } transition-all duration-300 block  h-14 w-full p-2 pl-4 text-xl text-neutral-900  bg-neutral-200 focus:ring-2 focus:ring-neutral-400 placeholder-neutral-600 dark:bg-neutral-800 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-neutral-50 dark:focus:ring-green-500 `}
+      } transition-all duration-300 block  h-14 w-full p-2 pl-4 text-xl text-neutral-900  bg-neutral-200 focus:ring-2 focus:ring-neutral-400 placeholder-neutral-600 dark:bg-neutral-800 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-neutral-50 dark:focus:ring-green-500 dark:focus:border-green-500`}
       placeholder={fieldName}
       value={value}
       onChange={(e) => setValue(e.target.value)}
