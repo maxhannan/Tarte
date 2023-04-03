d4e4890abe08eb2c8a78364faf563f17f14c6121
maxhannan <maxhannan@substanceseven.com>
2023-04-02 22:16:30 -0500

    form styling;

--- app/components/recipeForm/IngredientsSection.tsx
+++ app/components/recipeForm/IngredientsSection.tsx
@@ -6,7 +6,7 @@ const IngredientSection = () => {
   return (
     <div className="grid grid-cols-5  gap-y-4 gap-x-2 w-full ">
       <div className="flex gap-x-2 col-span-5">
-        <div className="grow h-14 inline-flex items-center  dark:bg-neutral-200 bg-neutral-300 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-100 dark:text-neutral-800  ">
+        <div className="grow h-14 inline-flex items-center  dark:bg-neutral-200 bg-neutral-800 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-100 dark:text-neutral-800  ">
           <h4 className="text-3xl  "> Ingredients</h4>
         </div>
       </div>
--- app/components/recipeForm/StepSection.tsx
+++ app/components/recipeForm/StepSection.tsx
@@ -9,13 +9,13 @@ const StepSection = ({ show }: { show: boolean }) => {
   return (
     <div className="grid grid-cols-5  gap-y-4 gap-x-2 w-full  z-20">
       <div className="flex gap-x-2 col-span-5">
-        <div className="grow h-14 inline-flex items-center  dark:bg-neutral-200 bg-neutral-300 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-100 dark:text-neutral-800  ">
+        <div className="grow h-14 inline-flex items-center  dark:bg-neutral-200 bg-neutral-800 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-100 dark:text-neutral-800  ">
           <h4 className="text-3xl  "> Steps</h4>
         </div>
       </div>
       <RecipeStep show={show} />
       <div className="col-span-5   h-12 inline-flex border-r-none items-center justify-between px-3 dark:bg-neutral-700 bg-neutral-200 transition-all duration-300 rounded-r-2xl   rounded-l-md rounded-bl-2xl text-lg text-neutral-700 dark:text-neutral-100  ">
-        <h4 className="text-xl dark:text-neutral-100 ">Add Ingredient</h4>
+        <h4 className="text-xl dark:text-neutral-100 ">Add Step</h4>
         <PlusCircleIcon className="h-7 w-7" />
       </div>
     </div>
