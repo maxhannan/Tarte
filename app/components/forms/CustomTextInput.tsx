09c63244a00ef2107597e1b7d6484a280d650814
maxhannan <maxhannan@substanceseven.com>
2023-04-02 22:03:13 -0500

    form styling;

--- app/components/forms/CustomTextInput.tsx
+++ app/components/forms/CustomTextInput.tsx
@@ -17,7 +17,7 @@ const CustomTextInput = ({
         value.length > 0
           ? "rounded-r-2xl rounded-l-md rounded-tl-2xl focus:ring-green-500  border-green-500 border-2 "
           : "rounded-r-2xl rounded-l-md rounded-bl-2xl focus:ring-red-500 border-none "
-      } transition-all duration-300 block  h-14 w-full p-2 pl-4 text-xl text-neutral-900 appearance-none  focus:ring-2 focus:outline-none focus:border-none bg-neutral-200    placeholder-neutral-600 dark:bg-neutral-800  dark:placeholder-neutral-400 dark:text-neutral-50 `}
+      } transition-all duration-300 block  h-14 w-full p-2 pl-4 text-xl text-neutral-200 appearance-none  focus:ring-2 focus:outline-none focus:border-none bg-neutral-700    placeholder-neutral-300 dark:bg-neutral-600  dark:placeholder-neutral-400 dark:text-neutral-50 `}
       placeholder={fieldName}
       value={value}
       onChange={(e) => setValue(e.target.value)}
--- app/components/recipeForm/IngredientsSection.tsx
+++ app/components/recipeForm/IngredientsSection.tsx
@@ -6,8 +6,8 @@ const IngredientSection = () => {
   return (
     <div className="grid grid-cols-5  gap-y-4 gap-x-2 w-full ">
       <div className="flex gap-x-2 col-span-5">
-        <div className="grow border-l-8 h-14 inline-flex items-center border-l-red-500   dark:bg-neutral-800 bg-neutral-200 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-700 dark:text-neutral-100  ">
-          <h4 className="text-3xl dark:text-neutral-100 "> Ingredients</h4>
+        <div className="grow h-14 inline-flex items-center  dark:bg-neutral-200 bg-neutral-300 transition-all duration-300 rounded-r-2xl   pl-3 font-light rounded-l-md rounded-bl-2xl text-lg text-neutral-100 dark:text-neutral-800  ">
+          <h4 className="text-3xl  "> Ingredients</h4>
         </div>
       </div>
       <IngredientAdder />
