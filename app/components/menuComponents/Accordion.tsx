ad427d6ccf28e71cf241e40e08a457ac3d7da13e
maxhannan <maxhannan@substanceseven.com>
2023-04-12 16:24:25 -0500

    accordions

--- app/components/menuComponents/Accordion.tsx
+++ app/components/menuComponents/Accordion.tsx
@@ -6,10 +6,10 @@ import MenuDishSummary from "./MenuDishSummary";
 export default function Accordion() {
   return (
     <div className=" rounded-tl-md rounded-2xl   border-neutral-300 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 p-2 flex flex-col gap-2">
-      <Disclosure>
+      <Disclosure defaultOpen>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
               <span>Appetizers</span>
               <ChevronUpIcon
                 className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
@@ -40,7 +40,7 @@ export default function Accordion() {
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
               <span>Appetizers</span>
               <ChevronUpIcon
                 className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
@@ -71,7 +71,7 @@ export default function Accordion() {
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
               <span>Appetizers</span>
               <ChevronUpIcon
                 className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
@@ -102,7 +102,100 @@ export default function Accordion() {
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+              <span>Appetizers</span>
+              <ChevronUpIcon
+                className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
+              />
+            </Disclosure.Button>
+
+            <Transition
+              enter="transition-all ease-linear duration-300  overflow-hidden"
+              enterFrom="transform opacity-0 max-h-0"
+              enterTo="transform opacity-100 max-h-96"
+              leave="transition-all ease-linear duration-200 overflow-hidden"
+              leaveFrom="transform opacity-100 max-h-96"
+              leaveTo="transform opacity-0 max-h-0"
+            >
+              <Disclosure.Panel className=" pb-2 flex flex-col gap-2 text-sm text-gray-500">
+                <MenuDishSummary
+                  id="1"
+                  name="Celeriac Soup, Hazelnut gremolata, Roasted Apple, Freekeh"
+                  category="4"
+                  user="MH"
+                />
+                <MenuDishSummary id="1" name="Test" category="4" user="MH" />
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+              <span>Appetizers</span>
+              <ChevronUpIcon
+                className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
+              />
+            </Disclosure.Button>
+
+            <Transition
+              enter="transition-all ease-linear duration-300  overflow-hidden"
+              enterFrom="transform opacity-0 max-h-0"
+              enterTo="transform opacity-100 max-h-96"
+              leave="transition-all ease-linear duration-200 overflow-hidden"
+              leaveFrom="transform opacity-100 max-h-96"
+              leaveTo="transform opacity-0 max-h-0"
+            >
+              <Disclosure.Panel className=" pb-2 flex flex-col gap-2 text-sm text-gray-500">
+                <MenuDishSummary
+                  id="1"
+                  name="Celeriac Soup, Hazelnut gremolata, Roasted Apple, Freekeh"
+                  category="4"
+                  user="MH"
+                />
+                <MenuDishSummary id="1" name="Test" category="4" user="MH" />
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
+              <span>Appetizers</span>
+              <ChevronUpIcon
+                className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
+              />
+            </Disclosure.Button>
+
+            <Transition
+              enter="transition-all ease-linear duration-300  overflow-hidden"
+              enterFrom="transform opacity-0 max-h-0"
+              enterTo="transform opacity-100 max-h-96"
+              leave="transition-all ease-linear duration-200 overflow-hidden"
+              leaveFrom="transform opacity-100 max-h-96"
+              leaveTo="transform opacity-0 max-h-0"
+            >
+              <Disclosure.Panel className=" pb-2 flex flex-col gap-2 text-sm text-gray-500">
+                <MenuDishSummary
+                  id="1"
+                  name="Celeriac Soup, Hazelnut gremolata, Roasted Apple, Freekeh"
+                  category="4"
+                  user="MH"
+                />
+                <MenuDishSummary id="1" name="Test" category="4" user="MH" />
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="dark:bg-opacity-50 flex w-full items-center justify-between rounded-tl-md rounded-xl  bg-neutral-200 font-normal dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 px-4 py-3 text-left text-xl  text-neutral-700 dark:text-neutral-200  focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75">
               <span>Appetizers</span>
               <ChevronUpIcon
                 className={`${open ? "rotate-180 transform" : ""} h-7 w-7 `}
--- app/components/menuComponents/MenuDishSummary.tsx
+++ app/components/menuComponents/MenuDishSummary.tsx
@@ -21,7 +21,7 @@ const MenuDishSummary = ({
       onClick={() => console.log(`/app/menus/${id}`)}
       className="  w-full max-h-full border-neutral-300 border  bg-neutral-100 rounded-2xl rounded-tl-md  py-4   flex justify-start items-center  px-4 hover:bg-neutral-300 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:border-neutral-700"
     >
-      <div className=" px-3 font-light">
+      <div className=" font-light">
         <h5 className="text-lg  text-neutral-700 dark:text-neutral-100">
           {name}
         </h5>
