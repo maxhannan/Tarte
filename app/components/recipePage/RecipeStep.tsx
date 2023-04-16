c6c39142038889457f4122b458af11adb8819bb0
maxhannan <maxhannan@substanceseven.com>
2023-04-16 18:29:33 -0500

    textsize

--- app/components/recipePage/RecipeStep.tsx
+++ app/components/recipePage/RecipeStep.tsx
@@ -12,13 +12,11 @@ const RecipeStep = ({
     <div
       onClick={() => setChecked(!checked)}
       className={`${
-        checked
-          ? "dark:bg-neutral-700 bg-neutral-300 border-l-8 border-l-green-500"
-          : "dark:bg-neutral-800 border-l-8 border-l-violet-500"
-      }  bg-neutral-200 transition-all duration-300 rounded-r-3xl p-6 font-light rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4`}
+        checked ? "dark:bg-neutral-700 bg-neutral-300" : "dark:bg-neutral-800 "
+      }  bg-neutral-200 transition-all duration-300 rounded-r-3xl p-6 rounded-l-md rounded-bl-3xl text-lg text-neutral-700 dark:text-neutral-100 mt-4`}
     >
       <h5 className="text-2xl mb-2">Step {stepNum}</h5>
-      <p className="text-base">{content}</p>
+      <p className="text-xl font-light ">{content}</p>
     </div>
   );
 };
