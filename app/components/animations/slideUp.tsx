c5366578670ea4de09635433bedacd0c7a6f9fe4
maxhannan <maxhannan@substanceseven.com>
2023-04-25 12:33:22 -0500

    testing new transitions

--- app/components/animations/slideUp.tsx
+++ app/components/animations/slideUp.tsx
@@ -5,7 +5,7 @@ const SlideUpTransition = ({ children }: { children: ReactNode }) => {
   return (
     <Transition
       enter="transition-all transform  ease-in-out  duration-300"
-      enterFrom=" opacity-0 translate-y-64 "
+      enterFrom=" opacity-0  "
       enterTo=" opacity-100 translate-y-0"
       leave="transition ease-in duration-100"
       leaveFrom="opacity-100"
