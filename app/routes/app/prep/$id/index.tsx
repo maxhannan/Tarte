ea4574a19df9ff50e867c1c5f20cb0c3658c756f
maxhannan <maxhannan@substanceseven.com>
2023-04-30 20:00:38 -0500

    bug fix

--- app/routes/app/prep/$id/index.tsx
+++ app/routes/app/prep/$id/index.tsx
@@ -1,10 +1,10 @@
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { DocumentPlusIcon } from "@heroicons/react/24/outline";
 import { Label } from "@radix-ui/react-label";
 import type { ActionFunction } from "@remix-run/node";
 import { Form, useActionData, useNavigation } from "@remix-run/react";
 import { Image, ImageIcon } from "lucide-react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import IconButton from "~/components/buttons/IconButton";
 
 import LoadingButton from "~/components/buttons/LoadingButton";
@@ -36,64 +36,83 @@ const PrepListPage = () => {
           },
         ]}
       />
-
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-50"
-      >
-        {" "}
-        <div className="fixed inset-0 bg-black/60" aria-hidden="true" />
-        {/* Full-screen container to center the panel */}
-        <div className="fixed inset-0 flex items-center justify-center">
-          <Dialog.Panel className="w-full max-w-md bg-neutral-100 dark:bg-transparent   rounded-xl p-4">
-            <div className="  carousel rounded-box  border border-neutral-200 dark:border-neutral-700  ">
-              <div id="item1" className="carousel-item w-full ">
-                <img
-                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/805c7a1c-cc08-447e-1f19-6c09211c3700/carousel"
-                  className="w-full object-cover"
-                  alt="Tailwind CSS Carousel component"
-                />
-              </div>
-              <div id="item2" className="carousel-item w-full ">
-                <img
-                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/a519da05-e2c7-48e7-dd9f-2cf425bf0d00/carousel"
-                  className="w-full object-cover"
-                  alt="Tailwind CSS Carousel component"
-                />
-              </div>
-              <div id="item3" className="carousel-item w-full ">
-                <img
-                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/eaedb384-b8ab-4639-bdaf-1c8d3e9efa00/carousel"
-                  className="w-full object-cover"
-                  alt="Tailwind CSS Carousel component"
-                />
-              </div>
-              <div id="item4" className="carousel-item w-full ">
-                <img
-                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/26dc6ed7-3023-47cf-4556-6e5c4400cf00/carousel"
-                  className="w-full object-cover"
-                  alt="Tailwind CSS Carousel component"
-                />
-              </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2  ">
-              <a href="#item1" className="btn btn-xs">
-                1
-              </a>
-              <a href="#item2" className="btn btn-xs">
-                2
-              </a>
-              <a href="#item3" className="btn btn-xs">
-                3
-              </a>
-              <a href="#item4" className="btn btn-xs">
-                4
-              </a>
-            </div>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="relative z-50"
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/60" aria-hidden="true" />
+          </Transition.Child>
+          <div className="fixed inset-0 flex items-center justify-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md bg-neutral-100 dark:bg-transparent   rounded-xl p-4">
+                <div className="  carousel rounded-box  border border-neutral-200 dark:border-neutral-700  ">
+                  <div id="item1" className="carousel-item w-full ">
+                    <img
+                      src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/805c7a1c-cc08-447e-1f19-6c09211c3700/carousel"
+                      className="w-full object-cover"
+                      alt="Tailwind CSS Carousel component"
+                    />
+                  </div>
+                  <div id="item2" className="carousel-item w-full ">
+                    <img
+                      src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/a519da05-e2c7-48e7-dd9f-2cf425bf0d00/carousel"
+                      className="w-full object-cover"
+                      alt="Tailwind CSS Carousel component"
+                    />
+                  </div>
+                  <div id="item3" className="carousel-item w-full ">
+                    <img
+                      src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/eaedb384-b8ab-4639-bdaf-1c8d3e9efa00/carousel"
+                      className="w-full object-cover"
+                      alt="Tailwind CSS Carousel component"
+                    />
+                  </div>
+                  <div id="item4" className="carousel-item w-full ">
+                    <img
+                      src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/26dc6ed7-3023-47cf-4556-6e5c4400cf00/carousel"
+                      className="w-full object-cover"
+                      alt="Tailwind CSS Carousel component"
+                    />
+                  </div>
+                </div>
+                <div className="flex justify-center w-full py-2 gap-2  ">
+                  <a href="#item1" className="btn btn-xs">
+                    1
+                  </a>
+                  <a href="#item2" className="btn btn-xs">
+                    2
+                  </a>
+                  <a href="#item3" className="btn btn-xs">
+                    3
+                  </a>
+                  <a href="#item4" className="btn btn-xs">
+                    4
+                  </a>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
       <div>
         <IconButton
           Icon={ImageIcon}
