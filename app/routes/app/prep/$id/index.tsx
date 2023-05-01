eafad5dd2ef3013bfe560df82428489136e97e02
maxhannan <maxhannan@substanceseven.com>
2023-04-30 20:28:02 -0500

    bug fix

--- app/routes/app/prep/$id/index.tsx
+++ app/routes/app/prep/$id/index.tsx
@@ -63,45 +63,36 @@ const PrepListPage = () => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md bg-transparent ">
-                <div className="carousel rounded-xl  min-w-full h-auto">
-                  <div
-                    id="item1"
-                    className="carousel-item w-full bg-neutral-900 "
-                  >
+              <Dialog.Panel className="w-full max-w-md bg-transparent  border border-neutral-800 rounded-xl p-4">
+                <div className="carousel rounded-box  min-w-full h-auto">
+                  <div id="item1" className="carousel-item w-full  ">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/805c7a1c-cc08-447e-1f19-6c09211c3700/carousel"
-                      className="object-contain "
+                      className="object-contain rounded-xl"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
-                  <div
-                    id="item2"
-                    className="carousel-item w-full bg-neutral-900"
-                  >
+                  <div id="item2" className="carousel-item w-full">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/a519da05-e2c7-48e7-dd9f-2cf425bf0d00/carousel"
-                      className="object-contain "
+                      className="object-contain rounded-xl"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
-                  <div
-                    id="item3"
-                    className="carousel-item w-full bg-neutral-900 "
-                  >
+                  <div id="item3" className="carousel-item w-full  ">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/eaedb384-b8ab-4639-bdaf-1c8d3e9efa00/carousel"
-                      className="object-contain "
+                      className="object-contain rounded-xl"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
                   <div
                     id="item4"
-                    className="carousel-item w-full  bg-neutral-900"
+                    className="carousel-item w-full rounded-xl over"
                   >
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/26dc6ed7-3023-47cf-4556-6e5c4400cf00/carousel"
-                      className="object-contain "
+                      className="object-contain rounded-xl"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
