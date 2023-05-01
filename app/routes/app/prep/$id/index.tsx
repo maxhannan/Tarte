58f0b88974e3c22241fc33794c49eaf8dd36692b
maxhannan <maxhannan@substanceseven.com>
2023-04-30 20:09:10 -0500

    bug fix

--- app/routes/app/prep/$id/index.tsx
+++ app/routes/app/prep/$id/index.tsx
@@ -65,46 +65,58 @@ const PrepListPage = () => {
             >
               <Dialog.Panel className="w-full max-w-md bg-neutral-100 dark:bg-transparent   rounded-xl p-4">
                 <div className="  carousel rounded-box  border border-neutral-200 dark:border-neutral-700  ">
-                  <div id="item1" className="carousel-item w-full ">
+                  <div id="item1" className="carousel-item w-full  ">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/805c7a1c-cc08-447e-1f19-6c09211c3700/carousel"
-                      className="w-full object-cover"
+                      className="object-contain"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
-                  <div id="item2" className="carousel-item w-full ">
+                  <div id="item2" className="carousel-item w-full w-full">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/a519da05-e2c7-48e7-dd9f-2cf425bf0d00/carousel"
-                      className="w-full object-cover"
+                      className="object-contain"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
-                  <div id="item3" className="carousel-item w-full ">
+                  <div id="item3" className="carousel-item w-full  ">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/eaedb384-b8ab-4639-bdaf-1c8d3e9efa00/carousel"
-                      className="w-full object-cover"
+                      className="object-contain"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
                   <div id="item4" className="carousel-item w-full ">
                     <img
                       src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/26dc6ed7-3023-47cf-4556-6e5c4400cf00/carousel"
-                      className="w-full object-cover"
+                      className="object-contain"
                       alt="Tailwind CSS Carousel component"
                     />
                   </div>
                 </div>
                 <div className="flex justify-center w-full py-2 gap-2  ">
-                  <a href="#item1" className="btn btn-xs">
+                  <a
+                    href="#item1"
+                    className="btn btn-sm bg-neutral-800 border border-neutral-700 rounded-xl active:bg-red-500 hover:bg-red-500"
+                  >
                     1
                   </a>
-                  <a href="#item2" className="btn btn-xs">
+                  <a
+                    href="#item2"
+                    className="btn btn-sm bg-neutral-800 border border-neutral-700 rounded-xl"
+                  >
                     2
                   </a>
-                  <a href="#item3" className="btn btn-xs">
+                  <a
+                    href="#item3"
+                    className="btn btn-sm bg-neutral-800 border border-neutral-700 rounded-xl"
+                  >
                     3
                   </a>
-                  <a href="#item4" className="btn btn-xs">
+                  <a
+                    href="#item4"
+                    className="btn btn-sm bg-neutral-800 border border-neutral-700 rounded-xl"
+                  >
                     4
                   </a>
                 </div>
