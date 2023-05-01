a315d45df2715ee7a5a8a261ea746cf4997532ce
maxhannan <maxhannan@substanceseven.com>
2023-04-30 19:15:21 -0500

    bug fix

--- app/routes/app/prep/$id/index.tsx
+++ app/routes/app/prep/$id/index.tsx
@@ -1,8 +1,11 @@
+import { Dialog } from "@headlessui/react";
 import { DocumentPlusIcon } from "@heroicons/react/24/outline";
 import { Label } from "@radix-ui/react-label";
 import type { ActionFunction } from "@remix-run/node";
 import { Form, useActionData, useNavigation } from "@remix-run/react";
-import { Image } from "lucide-react";
+import { Image, ImageIcon } from "lucide-react";
+import { useState } from "react";
+import IconButton from "~/components/buttons/IconButton";
 
 import LoadingButton from "~/components/buttons/LoadingButton";
 import CustomTextInput from "~/components/forms/CustomTextInput";
@@ -19,6 +22,7 @@ export const action: ActionFunction = async ({ request }) => {
 const PrepListPage = () => {
   const navigation = useNavigation();
   const response = useActionData();
+  const [isOpen, setIsOpen] = useState(true);
   console.log(response);
   return (
     <div>
@@ -32,41 +36,62 @@ const PrepListPage = () => {
           },
         ]}
       />
-      <div></div>
-      <div className="max-w-sm container mx-auto carousel rounded-box border border-neutral-700 mb-2 ">
-        <div id="item1" className="carousel-item w-full ">
-          <img
-            src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/805c7a1c-cc08-447e-1f19-6c09211c3700/carousel"
-            className="object-contain"
-            alt="Tailwind CSS Carousel component"
-          />
+
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        {" "}
+        <div className="fixed inset-0 bg-black/60" aria-hidden="true" />
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 flex items-center justify-center">
+          <Dialog.Panel className="w-full max-w-md bg-neutral-100 dark:bg-transparent  rounded-xl p-4">
+            <div className=" container mx-auto carousel rounded-box  border border-neutral-200 dark:border-neutral-700 ">
+              <div id="item1" className="carousel-item w-full ">
+                <img
+                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/805c7a1c-cc08-447e-1f19-6c09211c3700/carousel"
+                  className="object-contain"
+                  alt="Tailwind CSS Carousel component"
+                />
+              </div>
+              <div id="item2" className="carousel-item w-full ">
+                <img
+                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/a519da05-e2c7-48e7-dd9f-2cf425bf0d00/carousel"
+                  className=" object-contain"
+                  alt="Tailwind CSS Carousel component"
+                />
+              </div>
+              <div id="item3" className="carousel-item w-full ">
+                <img
+                  src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/26dc6ed7-3023-47cf-4556-6e5c4400cf00/carousel"
+                  className=" object-contain"
+                  alt="Tailwind CSS Carousel component"
+                />
+              </div>
+            </div>
+            <div className="flex justify-center w-full py-2 gap-2  ">
+              <a href="#item1" className="btn btn-xs">
+                1
+              </a>
+              <a href="#item2" className="btn btn-xs">
+                2
+              </a>
+              <a href="#item3" className="btn btn-xs">
+                3
+              </a>
+            </div>
+          </Dialog.Panel>
         </div>
-        <div id="item2" className="carousel-item w-full ">
-          <img
-            src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/a519da05-e2c7-48e7-dd9f-2cf425bf0d00/carousel"
-            className=" object-contain"
-            alt="Tailwind CSS Carousel component"
-          />
-        </div>
-        <div id="item3" className="carousel-item w-full ">
-          <img
-            src="https://imagedelivery.net/0xmRpZUcpgGyk0D6fR-N6A/26dc6ed7-3023-47cf-4556-6e5c4400cf00/carousel"
-            className=" object-contain"
-            alt="Tailwind CSS Carousel component"
-          />
-        </div>
+      </Dialog>
+      <div>
+        <IconButton
+          Icon={ImageIcon}
+          buttonName="openModa"
+          action={() => setIsOpen(!isOpen)}
+        />
       </div>
-      <div className="flex justify-center w-full py-2 gap-2  ">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-      </div>
+
       <Form method="post" encType="multipart/form-data">
         <div className="grid w-full max-w-sm items-center gap-1 text-neutral-200 mb-2">
           <Label htmlFor="picture" className="text-neutral-200">
