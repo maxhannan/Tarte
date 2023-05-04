a095f5501c47a3ab6f88663c17f60d79dddd9b56
maxhannan <maxhannan@substanceseven.com>
2023-05-03 21:45:20 -0500

    bug fix

--- app/components/recipeForm/recipeForm.tsx
+++ app/components/recipeForm/recipeForm.tsx
@@ -92,7 +92,7 @@ const RecipeForm = ({
                   <div className="relative  w-[80px] h-[56px] overflow-hidden  rounded-xl ">
                     <img
                       className=" object-cover"
-                      src={image}
+                      src={image.replace("carousel", "w-20")}
                       alt="Default avatar"
                     />
                   </div>
