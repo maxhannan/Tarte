4e7b283c81f465314c19b0b6b931dc877025071d
maxhannan <maxhannan@substanceseven.com>
2023-05-05 21:59:09 -0500

    bug fix

--- app/components/prep/PrepListItem.tsx
+++ app/components/prep/PrepListItem.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const PrepListItem = ({ name, unit }: Props) => {
   return (
-    <div className="  w-full max-h-full bg-neutral-100 border-neutral-300 border bg-opacity-50 dark:bg-opacity-50   rounded-xl  py-4  flex justify-start items-center  px-4 hover:bg-neutral-300 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:border-neutral-700">
+    <div className="  w-full max-h-full bg-neutral-100 border-neutral-300 border bg-opacity-50 dark:bg-opacity-50   rounded-xl  py-4  grid grid-cols-12 justify-start items-center  px-4 hover:bg-neutral-300 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:border-neutral-700">
       <div className=" font-light col-span-6">
         <h5 className="text-lg text-neutral-700 dark:text-neutral-100 ">
           {name}
