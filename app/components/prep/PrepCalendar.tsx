2e901c7ad49366961e31c7498d66e83b43bb64a4
maxhannan <maxhannan@substanceseven.com>
2023-05-05 22:45:36 -0500

    building out preplist page ans styling changes

--- app/components/prep/PrepCalendar.tsx
+++ app/components/prep/PrepCalendar.tsx
@@ -4,41 +4,71 @@ import { CalendarCheck2 } from "lucide-react";
 
 import { cn } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
-import { Calendar } from "~/components/ui/calendar";
+
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "~/components/ui/popover";
+import { Dialog, Transition } from "@headlessui/react";
+import { Calendar } from "../ui/calendar";
 
 export function PrepCalendar() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            " justify-start text-left text-lg   font-light pl-3 pr-4 border-neutral-300 dark:border-neutral-700 h-12 rounded-xl  text-neutral-700 hover:bg-neutral-200 hover:dark:bg-neutral-700 dark:text-neutral-200 bg-opacity-50 dark:bg-opacity-50 hover:dark:text-neutral-200 max-w-sm bg-neutral-200 dark:bg-neutral-800",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarCheck2 className="w-5 h-5 text-neutral-500 dark:text-neutral-400 mr-2" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent
-        align="end"
-        className="w-full bg-transparent p-0 border-none rounded-lg  "
+    <>
+      <Button
+        variant={"outline"}
+        onClick={() => setIsOpen(true)}
+        className={cn(
+          " justify-start text-left text-lg   font-light pl-3 pr-4 border-neutral-300 dark:border-neutral-700 h-12 rounded-xl  text-neutral-700 hover:bg-neutral-200 hover:dark:bg-neutral-700 dark:text-neutral-200 bg-opacity-50 dark:bg-opacity-50 hover:dark:text-neutral-200 max-w-sm bg-neutral-200 dark:bg-neutral-800",
+          !date && "text-muted-foreground"
+        )}
       >
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+        <CalendarCheck2 className="w-5 h-5 text-neutral-500 dark:text-neutral-400 mr-2" />
+        {date ? format(date, "PPP") : <span>Pick a date</span>}
+      </Button>
+      <Transition appear show={isOpen} as={React.Fragment}>
+        <Dialog
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="relative z-50"
+        >
+          <Transition.Child
+            as={React.Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/90" aria-hidden="true" />
+          </Transition.Child>
+          <div className="fixed inset-0 flex items-center justify-center p-4">
+            <Transition.Child
+              as={React.Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className=" max-w-xl bg-transparent">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  initialFocus
+                />
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   );
 }
