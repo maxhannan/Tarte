061d16da15f33f3a8795f407412e964f95e50339
Max Hannan <maxhannan@Maxs-Mac-mini.local>
2023-05-13 21:52:39 -0500

    updating prep lists!!

--- app/components/prep/PrepListItem.tsx
+++ app/components/prep/PrepListItem.tsx
@@ -1,8 +1,12 @@
-import { CheckBadgeIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowLongRightIcon,
+  ArrowRightIcon,
+  CheckBadgeIcon,
+} from "@heroicons/react/24/outline";
 
 import PrepListInput from "./PrepListInput";
 import { PrepItem } from "~/routes/app/prep/$id";
-import { Form, useFetcher } from "@remix-run/react";
+import { Form, NavLink, useFetcher } from "@remix-run/react";
 import { useRef } from "react";
 
 interface Props {
@@ -11,6 +15,7 @@ interface Props {
 
 const PrepListItem = ({ prepItem }: Props) => {
   const fetcher = useFetcher();
+
   const formRef = useRef<HTMLFormElement>(null);
   async function handleSubmit() {
     fetcher.submit(formRef.current!);
@@ -20,9 +25,22 @@ const PrepListItem = ({ prepItem }: Props) => {
       <div className="  max-w-full  bg-neutral-100 border-neutral-300 border bg-opacity-50 dark:bg-opacity-50   rounded-xl  py-2 px-2 grid grid-cols-10 gap-1   dark:bg-neutral-800 dark:border-neutral-700">
         <div className=" font-light col-span-5 lg:col-span-7 flex gap-2 items-center mr-2">
           <div>
-            <h5 className="text-sm lg:text-lg text-neutral-700 dark:text-neutral-100 ">
-              {prepItem.name}
-            </h5>
+            {prepItem.linkRecipeId ? (
+              <NavLink
+                to={`/app/recipes/${prepItem.linkRecipeId}`}
+                className="flex gap-2 items-center cursor-pointer hover:opacity-80"
+              >
+                <h5 className="text-sm lg:text-lg text-violet-500 dark:text-violet-400 ">
+                  {prepItem.name}
+                </h5>
+                <ArrowLongRightIcon className="text-violet-500 dark:text-violet-400  w-5 h-5" />
+              </NavLink>
+            ) : (
+              <h5 className="text-sm lg:text-lg text-neutral-700 dark:text-neutral-100 ">
+                {prepItem.name}
+              </h5>
+            )}
+
             <h6 className="text-sm text-neutral-700 dark:text-neutral-100 ">
               {`(${prepItem.prepUnit})`}
             </h6>
